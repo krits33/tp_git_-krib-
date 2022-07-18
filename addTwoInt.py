f2d7fd03aaa94efd53908d72cd98baf3dfe1fbe2
krits33 <kritikabissessur03@gmail.com>
2022-07-18 15:17:31 +0400

    [add]python addTwoInt.py modified

--- addTwoInt.py
+++ addTwoInt.py
@@ -6,8 +6,12 @@ print(sys.argv)
 
 def add(a,b):
       return a+b
+#nombre_d'arg correcte
+#sinon l'affichage d'un erreur
+nombre_d'arg=len(sys.argv)-1
+if (nombre_d'arg <2):
+             print("Error detected")
 
-#main
 x=int( sys.argv[1] )
 y=int( sys.argv[2] )
 
